da4c063eae7af9c818a1b227258eacedb28e5a8f
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-05 13:54:34 +0000

    Add test for backwards pawn moves - RED

--- tests/ChessBackend.Tests/Pieces/PawnTests.cs
+++ tests/ChessBackend.Tests/Pieces/PawnTests.cs
@@ -30,4 +30,22 @@ public class PawnTests
 
     Assert.That(pawn.validateMove((1, 4), (3, 4), board), Is.EqualTo(true));
   }
+
+  [Test]
+  public void Pawn_ValidateMove_ShouldReturnFalse_ForWhiteBackwardsMove()
+  {
+    var board = new Board();
+    var pawn = new Pawn("White");
+
+    Assert.That(pawn.validateMove((3, 4), (2, 4), board), Is.EqualTo(false));
+  }
+
+  [Test]
+  public void Pawn_ValidateMove_ShouldReturnFalse_ForBlackBackwardsMove()
+  {
+    var board = new Board();
+    var pawn = new Pawn("Black");
+
+    Assert.That(pawn.validateMove((4, 4), (5, 4), board), Is.EqualTo(false));
+  }
 }
