f09f7d90b141d1245b12029838dcd76a5eb2c148
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-06 12:18:53 +0000

    Add test for pawn moving 2 squares jumping over piece - RED

--- tests/ChessBackend.Tests/Pieces/PawnTests.cs
+++ tests/ChessBackend.Tests/Pieces/PawnTests.cs
@@ -32,6 +32,26 @@ public class PawnTests
   }
 
   [Test]
+  public void Pawn_ValidateMove_ShouldReturnFalse_ForTwoSquaresOnFirstMoveIfFirstSquareIsOccupied()
+  {
+    var position = new string[8, 8]
+    {
+      {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
+      {"bp", "bp", "bp", "bp", "oo", "bp", "bp", "bp"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "bp", "oo", "oo", "oo"},
+      {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
+      {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
+    };
+    var board = new Board(position);
+    var pawn = new Pawn(Colour.White);
+
+    Assert.That(pawn.validateMove("e2e4", board), Is.EqualTo(false));
+  }
+
+  [Test]
   public void Pawn_ValidateMove_ShouldReturnFalse_ForWhiteBackwardsMove()
   {
     var board = new Board();
