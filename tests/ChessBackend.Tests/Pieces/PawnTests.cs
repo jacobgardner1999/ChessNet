ff322e821b29b308d781dccbf39cfe74add77b27
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-06 10:40:43 +0000

    Add test for blockaded pawns - RED

--- tests/ChessBackend.Tests/Pieces/PawnTests.cs
+++ tests/ChessBackend.Tests/Pieces/PawnTests.cs
@@ -48,4 +48,25 @@ public class PawnTests
 
     Assert.That(pawn.validateMove((3, 4), (2, 4), board), Is.EqualTo(false));
   }
+
+  [Test]
+  public void Pawn_ValidateMove_ShouldReturnFalse_IfSquareIsOccupied()
+  {
+    var position = new string[8, 8]
+    {
+      {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
+      {"bp", "bp", "bp", "bp", "oo", "bp", "bp", "bp"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "bp", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "wp", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"wp", "wp", "wp", "wp", "oo", "wp", "wp", "wp"},
+      {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
+    };
+
+    var board = new Board(position);
+    var pawn = new Pawn(Colour.White);
+
+    Assert.That(pawn.validateMove((4, 4), (3, 4), board), Is.EqualTo(false));
+  }
 }
