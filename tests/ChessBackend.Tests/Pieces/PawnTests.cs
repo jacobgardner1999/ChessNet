1a6e5e92c3ee9a319a4bf162249edb9a90aa67d5
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-06 12:07:51 +0000

    Add tests for pawn captures - RED

--- tests/ChessBackend.Tests/Pieces/PawnTests.cs
+++ tests/ChessBackend.Tests/Pieces/PawnTests.cs
@@ -69,4 +69,70 @@ public class PawnTests
 
     Assert.That(pawn.validateMove("e4e5", board), Is.EqualTo(false));
   }
+
+  [Test]
+  public void Pawn_ValidateMove_ShouldReturnTrue_ForWhiteCapture()
+  {
+    var position = new string[8, 8]
+    {
+      {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
+      {"bp", "bp", "bp", "bp", "oo", "bp", "bp", "bp"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "bp", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "wp", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"wp", "wp", "wp", "wp", "oo", "wp", "wp", "wp"},
+      {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
+    };
+
+    var board = new Board(position);
+    var pawn = new Pawn(Colour.White);
+
+    Assert.That(pawn.validateMove("e4d5", board), Is.EqualTo(true));
+
+  }
+
+  [Test]
+  public void Pawn_ValidateMove_ShouldReturnTrue_ForBlackCapture()
+  {
+    var position = new string[8, 8]
+    {
+      {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
+      {"bp", "bp", "bp", "bp", "oo", "bp", "bp", "bp"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "bp", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "wp", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"wp", "wp", "wp", "wp", "oo", "wp", "wp", "wp"},
+      {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
+    };
+
+    var board = new Board(position);
+    var pawn = new Pawn(Colour.Black);
+
+    Assert.That(pawn.validateMove("d5e4", board), Is.EqualTo(true));
+
+  }
+
+  [Test]
+  public void Pawn_ValidateMove_ShouldReturnFalse_ForCaptureOnEmptySquare()
+  {
+    var position = new string[8, 8]
+    {
+      {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
+      {"bp", "bp", "bp", "bp", "oo", "bp", "bp", "bp"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "bp", "oo", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "wp", "oo", "oo", "oo"},
+      {"oo", "oo", "oo", "oo", "oo", "oo", "oo", "oo"},
+      {"wp", "wp", "wp", "wp", "oo", "wp", "wp", "wp"},
+      {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"},
+    };
+
+    var board = new Board(position);
+    var pawn = new Pawn(Colour.Black);
+
+    Assert.That(pawn.validateMove("e4f5", board), Is.EqualTo(false));
+
+  }
 }
