a44d245e49143d7c3cb4a73911b4458b704e9b51
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-10 10:09:00 +0000

    Add basic king movement - GREEN

--- src/ChessBackend/Pieces/King.cs
+++ src/ChessBackend/Pieces/King.cs
@@ -14,6 +14,19 @@ public class King : IPiece
 
   public bool validateMove((int row, int col) position, (int row, int col) target, IBoard board)
   {
-    return true;
+    if (Math.Abs(position.row - target.row) < 2
+        && Math.Abs(position.col - target.col) < 2
+        && board.GetPieceAt(target.row, target.col).Colour != Colour)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  public bool validateMove(string move, IBoard board)
+  {
+    var (position, target) = board.ParseMove(move);
+
+    return validateMove((position[0], position[1]), (target[0], target[1]), board);
   }
 }
