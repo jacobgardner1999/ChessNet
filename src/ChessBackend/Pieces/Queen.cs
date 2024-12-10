1af787e1ab5abe7d52c4af9a6612a60c65f3c9b0
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-10 12:57:22 +0000

    Add Queen movement - GREEN

--- src/ChessBackend/Pieces/Queen.cs
+++ src/ChessBackend/Pieces/Queen.cs
@@ -14,7 +14,52 @@ public class Queen : IPiece
 
   public bool validateMove((int row, int col) position, (int row, int col) target, IBoard board)
   {
-    return true;
+    if (position.col == target.col
+        && board.GetPieceAt(target.row, target.col).Colour != Colour)
+    {
+      var dy = (position.row > target.row) ? -1 : 1;
+      for (var i = position.row + dy; i != target.row; i += dy)
+      {
+        if (board.IsSquareOccupied(i, target.col))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    if (position.row == target.row
+        && board.GetPieceAt(target.row, target.col).Colour != Colour)
+    {
+      var dx = (position.col > target.col) ? -1 : 1;
+      for (var i = position.col + dx; i != target.col; i += dx)
+      {
+        if (board.IsSquareOccupied(target.row, i))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    if (Math.Abs(position.row - target.row) == Math.Abs(position.col - target.col)
+        && board.GetPieceAt(target.row, target.col).Colour != Colour)
+    {
+      var dx = (position.row > target.row) ? -1 : 1;
+      var dy = (position.col > target.col) ? -1 : 1;
+      var j = position.col + dy;
+      for (var i = position.row + dx; i != target.row; i += dx)
+      {
+        if (board.IsSquareOccupied(i, j))
+        {
+          return false;
+        }
+        j += dy;
+      }
+      return true;
+    }
+
+    return false;
   }
 
   public bool validateMove(string move, IBoard board)
