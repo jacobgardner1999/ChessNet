c2ac2f5f6b4d419a3b13bdd76907a170d8728fed
Jacob Gardner <jacobjamesg1999@gmail.com>
2024-12-10 11:34:29 +0000

    Add rook movement - GREEN

--- src/ChessBackend/Pieces/Rook.cs
+++ src/ChessBackend/Pieces/Rook.cs
@@ -14,6 +14,40 @@ public class Rook : IPiece
 
   public bool validateMove((int row, int col) position, (int row, int col) target, IBoard board)
   {
-    return true;
+    if (position.col == target.col
+        && board.GetPieceAt(target.row, target.col).Colour != Colour)
+    {
+      var dy = (position.row > target.row) ? -1 : 1;
+      for (var i = position.row + dy; i != target.row; i += dy)
+      {
+        if (board.IsSquareOccupied(i, target.col))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    if (position.row == target.row
+        && board.GetPieceAt(target.row, target.col).Colour != Colour)
+    {
+      var dx = (position.col > target.col) ? -1 : 1;
+      for (var i = position.col + dx; i != target.col; i += dx)
+      {
+        if (board.IsSquareOccupied(target.row, i))
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  public bool validateMove(string move, IBoard board)
+  {
+    var (position, target) = board.ParseMove(move);
+
+    return validateMove((position[0], position[1]), (target[0], target[1]), board);
   }
 }
